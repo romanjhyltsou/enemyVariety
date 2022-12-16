34bce7af9c2515b1afa6b560aab3e5f17bceeb7f
romanjhyltsou <romanjhyltsou@gmail.com>
2022-12-16 14:21:06 +0300

    init: start enemyVariety

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d'); 
+canvas.width = 500;
+canvas.height = 800;
+
+
+    
+/*     function animate() {
+      
+    }
+    animate(); */
